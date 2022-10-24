0b782de5a01cc3663bd368ff2314c4d8c34c0654
Joey <joeysmccracken@gmail.com>
2022-10-24 20:12:02 +0100

    included public metrics and converts to .csv

--- query.sh
+++ query.sh
@@ -4,7 +4,9 @@ TOKEN='AAAAAAAAAAAAAAAAAAAAAILWiQEAAAAAIGSBaJbgx87CDIVonfXs3eASnEA%3DOz08lEqqTu3
 
 QUERY=`python httpEncoder.py $1`
 
-curl --request GET "https://api.twitter.com/2/tweets/search/recent?query=${QUERY}&max_results=10" --header "Authorization: Bearer ${TOKEN}"
+curl --request GET "https://api.twitter.com/2/tweets/search/recent?query=${QUERY}&max_results=10&tweet.fields=author_id,created_at,public_metrics" --header "Authorization: Bearer ${TOKEN}" > response.json
+
+in2csv -k data response.json > response.csv
 
 
 
